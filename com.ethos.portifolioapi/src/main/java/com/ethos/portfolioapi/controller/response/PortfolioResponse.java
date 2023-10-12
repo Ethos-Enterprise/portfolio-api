6eb0bc98829bb078e18bcbaa5af101f12cb59284
VitXP <vitor.pereira@sptech.school>
2023-10-12 17:37:13 -0300

    Refactor: Remoção de pasta duplicada

--- com.ethos.portifolioapi/src/main/java/com/ethos/portfolioapi/controller/response/PortfolioResponse.java
+++
@@ -1,8 +0,0 @@
-package com.ethos.portfolioapi.controller.response;
-
-import java.time.LocalDate;
-import java.util.UUID;
-
-public record PortfolioResponse(UUID id, String urlImagemPerfil, String urlBackgroundPerfil, String descricaoEmpresa, String sobreEmpresa, String linkWebsiteEmpresa, LocalDate dataEmpresaCertificada, String fkPrestadoraServico){
-
-}
