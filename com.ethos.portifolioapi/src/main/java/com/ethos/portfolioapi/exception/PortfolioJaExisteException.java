4daf4cea0a9d90a801b5a960db4b73258b84513e
lealivia <livia.leal@sptech.school>
2023-10-19 17:16:43 -0300

    Refactor - mudança de nomenclatura de métodos

--- com.ethos.portifolioapi/src/main/java/com/ethos/portfolioapi/exception/PortfolioJaExisteException.java
+++
@@ -1,5 +0,0 @@
-package com.ethos.portfolioapi.exception;
-
-public class PortfolioJaExisteException extends RuntimeException{
-    public PortfolioJaExisteException(String message) { super(message); }
-}
