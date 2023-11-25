5791d5e9e887f79b3b440e3c053ff6abfbc8630a
lealivia <livia.leal@sptech.school>
2023-11-25 20:38:42 -0300

    Fix: Mudando atributos da requisição

--- com.ethos.portfolioapi/src/main/java/com/ethos/portfolioapi/controller/request/PortfolioRequest.java
+++ com.ethos.portfolioapi/src/main/java/com/ethos/portfolioapi/controller/request/PortfolioRequest.java
@@ -7,8 +7,7 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record PortfolioRequest(@NotBlank(message = "Erro ao importar imagem") String urlImagemPerfil,
-                               @NotBlank(message = "Erro ao importar imagem background") String urlBackgroundPerfil,
+public record PortfolioRequest(
                                @NotBlank(message = "Descrição em branco") @Size(min=1, max=300, message = "O campo sobre esta fora do limite de caracteres") String descricaoEmpresa,
                                @NotBlank(message = "Texto sobre em branco") @Size(min=1, max=1000, message = "O campo sobre esta fora do limite de caracteres") String sobreEmpresa,
                                @NotBlank(message = "Link inválido") String linkWebsiteEmpresa,
