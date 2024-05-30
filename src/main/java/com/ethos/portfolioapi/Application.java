858853a8e19816de2515a53cb3a156fbd17dc3b6
lealiv <livia.lealxx@gmail.com>
2024-05-29 21:44:29 -0300

    Feat: Adicionando Teste Watchtower

--- src/main/java/com/ethos/portfolioapi/Application.java
+++ src/main/java/com/ethos/portfolioapi/Application.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("Teste watchtower");
 		SpringApplication.run(Application.class, args);
 	}
 
